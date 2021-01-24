ff1e4a160ceeb5c01eb82b2b469821d5987d84e7
Sylvester Tamba <sylvestertamba@gmail.com>
2021-01-24 07:03:06 +0300

    Create Login and Register System with C# Winforms

---
+++ LoginSysten/LoginSysten/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginSysten
+{
+    public partial class Form1 : Form
+    {
+
+        // Initialize the connection class
+        Config db = new Config();
+        public Form1()
+        {
+            InitializeComponent();
+
+            // pass the database you want to connect to
+            db.Connect("userdata");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // querry MySQL database for the data passed from textboxes
+            db.ExecuteSelect("SELECT * FROM `user_info` where username='" + textBox1.Text+ "' and password ='" + textBox2.Text+"'");
+
+            if (db.Count() == 1)
+            {
+                MessageBox.Show("Success You will Login as"+db.Results(0, "names"));
+            }
+            else
+            {
+                MessageBox.Show("Wrong username and password combination" );
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // start register window
+            Register register = new Register();
+
+            register.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            // closes the application
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ LoginSysten/LoginSysten/Register.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginSysten
+{
+    public partial class Register : Form
+    {
+        Config db = new Config();
+        public Register()
+        {
+            InitializeComponent();
+            // pass the database you want to connect to
+            db.Connect("userdata");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // saves data in the database
+            db.Execute("INSERT INTO `user_info` (`id`, `names`, `username`, `password`) VALUES (NULL, '"+textBox3.Text+"', '"+textBox1.Text+"', '"+textBox2.Text+"');");
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // closses the register window
+            this.Close();
+        }
+    }
+}
